968dcd749ce8ae5f70d4a845aa10595f0625cbf5
Alena Dzhukich <github@alena.moe>
2018-10-19 23:00:23 +0300

    Bootstrap project

---
+++ src/Root/Root.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+class Root extends React.PureComponent {
+	public render() {
+		return (
+			<div>Root</div>
+		)
+	}
+}
+
+export default Root
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import Root from './Root';
+
+ReactDOM.render(
+	<Root />,
+	document.getElementById('root') as HTMLElement
+);
