e68029a0813f665023d7d273e219d97ac02232a2
Alena Dzhukich <github@alena.moe>
2018-10-20 04:03:26 +0300

    Add draft of emboss formatter

--- src/FormattedEmboss/FormattedEmboss.tsx
+++ src/FormattedEmboss/FormattedEmboss.tsx
@@ -4,11 +4,52 @@ import { copyToClipboard } from '@helpers'
 import { FormattedEmbossProps } from './FormattedEmboss.d'
 import Root from './styles'
 
+const formatEmboss = (initialEmboss: string): string => {
+	// Alena Dzhukich -> Alena Dzhukich
+	if (initialEmboss.length <= 21) {
+		return initialEmboss
+	}
+
+	const [firstName, lastName] = initialEmboss.split(' ');
+	const [first, second] = lastName.split('-');
+
+	// Alena Dzhukich -> A Dzhukich
+	if ((lastName && lastName.length) + 2 <= 21) {
+		return `${firstName.charAt(0)} ${lastName}`;
+	}
+
+	// Alena Dzhukich-Solodova -> Alena Dzhukich
+	if ((first && first.length) + firstName.length + 1 <= 21) {
+		return `${firstName} ${first}`
+	}
+
+	// Alena Dzhukich-Solodova -> Alena Solodova
+	if ((second && second.length) + firstName.length + 1 <= 21) {
+		return `${firstName} ${second}`
+	}
+
+	// Alena Dzhukich-Solodova -> A Dzhukich
+	if ((first && first.length) + 2 <= 21) {
+		return `${firstName.charAt(0)} ${first}`
+	}
+
+	// Alena Dzhukich-Solodova -> A Solodova
+	if ((second && second.length) + 2 <= 21) {
+		return `${firstName.charAt(0)} ${second}`
+	}
+
+	return 'Bad name =('
+}
+
 class FormattedEmboss extends React.PureComponent<FormattedEmbossProps> {
+	public static propTypes = {
+		emboss: '',
+	}
+
 	public copyEmbossHd = (): void => {
 		const { emboss } = this.props
 
-		copyToClipboard(emboss.toUpperCase())
+		copyToClipboard(formatEmboss(emboss).toUpperCase())
 
 		this.props.addPush({
 			title: 'Copied',
@@ -19,7 +60,7 @@ class FormattedEmboss extends React.PureComponent<FormattedEmbossProps> {
 		const { emboss } = this.props
 
 		return (
-			<Root onClick={this.copyEmbossHd}>{ emboss.toUpperCase() }</Root>
+			<Root onClick={this.copyEmbossHd}>{ formatEmboss(emboss).toUpperCase() }</Root>
 		)
 	}
 }
