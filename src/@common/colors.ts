dae3911c4b6ff60950484975a86f2855357becc1
Alena Dzhukich <github@alena.moe>
2018-10-23 18:37:02 +0300

    Remove unused vars && make font elastic

--- src/@common/colors.ts
+++ src/@common/colors.ts
@@ -3,22 +3,10 @@ import { createGlobalStyle } from 'styled-components'
 export default createGlobalStyle`
 	:root {
 		--swan-white: #F5F6FA;
-		--hot-stone: #aaa69d;
-
 		--purple: #9c88ff;
 		--liberty: #8c7ae6;
 
-		--summer-sky: #00a8ff;
-		--devil-blue: #227093;
-
 		--celestial-green: #4cd137;
-		--palm-springs-splash: #218C74;
-
-		--red: #FF5252;
-		--eye-of-newt: #B33939;
-
-		--pumpkin: #FF793F;
-		--chilean-fire: #CD6133;
 
 		--mandarin-sorbet: #FFB142;
 	}
--- src/@common/general.ts
+++ src/@common/general.ts
@@ -2,7 +2,19 @@ import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
 	:root {
-		font-size: 4px;
+		font-size: 0.6vw;
+
+		@media screen and (min-width: 640px) {
+			font-size: 0.3vw;
+		}
+
+		@media screen and (min-width: 1024px) {
+			font-size: 0.25vw;
+		}
+
+		@media screen and (min-width: 1680px) {
+			font-size: 0.2vw;
+		}
 	}
 
 	body {
